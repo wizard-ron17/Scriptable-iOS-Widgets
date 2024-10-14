6e68940de21c1467c1754211727ef25642f8cda8
ron <128203911+wizard-ron17@users.noreply.github.com>
2024-10-14 10:41:30 -0400

    Update broPrice.js

--- Kadena Tokens/broPrice.js
+++ Kadena Tokens/broPrice.js
@@ -1,6 +1,3 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: light-brown; icon-glyph: handshake;
 const params = args.widgetParameter ? args.widgetParameter.split(",") : [];
 
 const isDarkTheme = params?.[0] === 'dark';
@@ -8,7 +5,7 @@ const padding = 10;
 
 const widget = new ListWidget();
 if (isDarkTheme) {
-  widget.backgroundColor = new Color('#1C1C1E');
+  widget.backgroundColor = new Color('#1C1C1E');
 }
 widget.setPadding(padding, padding, padding, padding);
 
@@ -19,64 +16,64 @@ headerStack.setPadding(0, 0, 0, 0);
 const headerText = headerStack.addText("$BRO Price");
 headerText.font = Font.semiboldSystemFont(18);
 if (isDarkTheme) {
-  headerText.textColor = new Color('#FFFFFF');
+  headerText.textColor = new Color('#FFFFFF');
 }
 
 async function buildWidget() {
-  const broImage = await loadImage('https://swap.ecko.finance/images/crypto/bro.png');
+  const broImage = await loadImage('https://raw.githubusercontent.com/CryptoPascal31/kadena_tokens/main/img/bro.png');
 
-  const broPriceInfo = await getTokenPriceInfo();
-  const broPrice = broPriceInfo.price;
-  
-  const usdRate = await getUSDExchangeRate();
-  const broPriceUSD = (parseFloat(broPrice) * parseFloat(usdRate)).toFixed(2);
-  
-  addCrypto(broImage, `${broPrice} KDA`, `$${broPriceUSD} USD`);
+  const broPriceInfo = await getTokenPriceInfo();
+  const broPrice = broPriceInfo.price;
+  
+  const usdRate = await getUSDExchangeRate();
+  const broPriceUSD = (parseFloat(broPrice) * parseFloat(usdRate)).toFixed(2);
+  
+  addCrypto(broImage, `${broPrice} KDA`, `$${broPriceUSD} USD`);
 }
 
 function addCrypto(image, kdaPrice, usdPrice) {
-  const rowStack = widget.addStack();
-  rowStack.setPadding(0, 0, 0, 0);
-  rowStack.layoutVertically();
+  const rowStack = widget.addStack();
+  rowStack.setPadding(0, 0, 0, 0);
+  rowStack.layoutVertically();
 
-  const imageStack = rowStack.addStack();
-  const priceStack = rowStack.addStack();
-  const priceUSDStack = rowStack.addStack();
+  const imageStack = rowStack.addStack();
+  const priceStack = rowStack.addStack();
+  const priceUSDStack = rowStack.addStack();
 
-  imageStack.setPadding(5, 25, 5, 0);
+  imageStack.setPadding(5, 25, 5, 0);
 
-  const imageNode = imageStack.addImage(image);
-  imageNode.imageSize = new Size(50, 50);
-  imageNode.leftAlignImage();
+  const imageNode = imageStack.addImage(image);
+  imageNode.imageSize = new Size(50, 50);
+  imageNode.leftAlignImage();
 
-  const kdaPriceText = priceStack.addText(kdaPrice);
-  kdaPriceText.font = Font.semiboldSystemFont(18);
-  if (isDarkTheme) {
-    kdaPriceText.textColor = new Color('#FFFFFF');
-  }
+  const kdaPriceText = priceStack.addText(kdaPrice);
+  kdaPriceText.font = Font.semiboldSystemFont(18);
+  if (isDarkTheme) {
+    kdaPriceText.textColor = new Color('#FFFFFF');
+  }
 
-  const usdPriceText = priceUSDStack.addText(usdPrice);
-  usdPriceText.font = Font.systemFont(16);
-  usdPriceText.textColor = new Color('#888888');
+  const usdPriceText = priceUSDStack.addText(usdPrice);
+  usdPriceText.font = Font.systemFont(16);
+  usdPriceText.textColor = new Color('#888888');
 }
 
 async function getTokenPriceInfo() {
-  const url = 'https://backend2.euclabs.net/kadena-indexer/v1/account/P1J6eqZteaU19umFecSjpEuc7Pea79SdDzz2Vp9LOpc'
-  const req = new Request(url)
-  const apiResult = await req.loadJSON();
-  return { price: (apiResult['assets'][0].totalBalance / apiResult['assets'][1].totalBalance).toFixed(2) };
+  const url = 'https://backend2.euclabs.net/kadena-indexer/v1/account/P1J6eqZteaU19umFecSjpEuc7Pea79SdDzz2Vp9LOpc'
+  const req = new Request(url)
+  const apiResult = await req.loadJSON();
+  return { price: (apiResult['assets'][0].totalBalance / apiResult['assets'][1].totalBalance).toFixed(2) };
 }
 
 async function getUSDExchangeRate() {
-  const url = 'https://api.coinbase.com/v2/exchange-rates?currency=KDA';
-  const req = new Request(url);
-  const apiResult = await req.loadJSON();
-  return apiResult['data']['rates']['USD'];
+  const url = 'https://api.coinpaprika.com/v1/tickers/kda-kadena';
+  const req = new Request(url);
+  const apiResult = await req.loadJSON();
+  return apiResult['quotes']['USD']['price'];
 }
 
 async function loadImage(imgUrl) {
-  const req = new Request(imgUrl);
-  return await req.loadImage();
+  const req = new Request(imgUrl);
+  return await req.loadImage();
 }
 
 await buildWidget();
